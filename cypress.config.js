6adfd4bf789205ee11b509202144836d2998f286
Marcus Harvey <marcus0228@gmail.com>
2023-07-20 18:43:13 +0200

    add Cypress test and workflow

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'reporter-config.json',
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
---
+++ cypress/support/e2e.js
@@ -0,0 +1,29 @@
+import './commands'
+
+// Mochawesome Report - Add screenshots and videos to the HTML report
+import addContext from 'mochawesome/addContext'
+
+const titleToFileName = function () {
+  title.replace(/[:/]/g, '')
+}
+
+Cypress.on('test:after:run', function (test, runnable) {
+  if (test.state === 'failed') {
+    let parent = runnable.parent
+    let filename = ''
+    while (parent && parent.title) {
+      filename = `${titleToFileName(
+        parent.title
+      )} -- ${filename}`
+      parent = parent.parent
+    }
+    filename += `${titleToFileName(
+      test.title
+    )} (failed).png`
+    addContext(
+      { test },
+      `../cypress/screenshots/${Cypress.spec.name}/${filename}`
+    )
+  }
+  addContext({ test }, `../cypress/videos/${Cypress.spec.name}.mp4`)
+})
