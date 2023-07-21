060c65d4bcaa64806c92f698778765be03f7fbce
Marcus Harvey <marcus0228@gmail.com>
2023-07-21 13:20:11 +0200

    testing Gleb reusable workflow

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-multi-reporters',
-  reporterOptions: {
-    configFile: 'reporter-config.json',
+  // reporter: 'cypress-multi-reporters',
+  // reporterOptions: {
+  //   configFile: 'reporter-config.json',
+  // },
+    reporter: 'mochawesome',
+    reporterOptions: {
+      useInlineDiffs: true,
+      embeddedScreenshots: true,
+      reportDir: 'cypress/results',
+      reportFilename: '[name].html',
+      overwrite: true,
+      html: true,
+      json: true,
   },
   e2e: {
     setupNodeEvents(on, config) {
